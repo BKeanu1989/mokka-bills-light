6c3a5b6a0c6492a516475f0eeee647990e8d3336
Kevin Fechner <developer.kevinfechner@gmail.com>
2018-02-02 01:18:54 +0100

    almost done? marge in productArray, TODO: sieb & artistDetails

--- includes/Abrechnung.php
+++ includes/Abrechnung.php
@@ -1,7 +1,11 @@
 <?php
 class Abrechnung {
+  //
   public $orderIDs = [];
+
   public $orderItemIDs = [];
+
+  public $orders = [];
   // artist_ids
   public $artists = [];
   public function __construct($from, $to) {
@@ -10,30 +14,37 @@ class Abrechnung {
 
   public function getOrdersInTimeFrame($from, $to) {
     global $wpdb;
-    $query = $wpdb->prepare("SELECT MAIN_POSTMETA.post_id, MAIN_POSTMETA.meta_value AS order_sold_at FROM wp_mokkamerch_postmeta AS MAIN_POSTMETA JOIN wp_mokkamerch_postmeta AS EMAIL_POSTMETA ON MAIN_POSTMETA.post_id = EMAIL_POSTMETA.post_id AND EMAIL_POSTMETA.meta_key = '_billing_email' AND EMAIL_POSTMETA.meta_value <> 'service@mokka-merch.com'  WHERE MAIN_POSTMETA.meta_key = '_order_sold' AND MAIN_POSTMETA.meta_value BETWEEN %s AND %s;", $from, $to);
-    echo $query;
+    $query = $wpdb->prepare("SELECT MAIN_POSTMETA.post_id AS order_id, MAIN_POSTMETA.meta_value AS order_sold_at FROM {$wpdb->prefix}postmeta AS MAIN_POSTMETA JOIN {$wpdb->prefix}postmeta AS EMAIL_POSTMETA ON MAIN_POSTMETA.post_id = EMAIL_POSTMETA.post_id AND EMAIL_POSTMETA.meta_key = '_billing_email' AND EMAIL_POSTMETA.meta_value <> 'service@mokka-merch.com'  WHERE MAIN_POSTMETA.meta_key = '_order_sold' AND MAIN_POSTMETA.meta_value BETWEEN %s AND %s;", $from, $to);
+    // echo $query;
     $results = $wpdb->get_results($query, ARRAY_A);
 
-    $orderIDs = array_map(function($orderID) {
-      return $orderID["post_id"];
-    }, $results);
-
-    $this->orderIDs = $orderIDs;
-    return $orderIDs;
+    $this->orderIDs = $results;
+    return $results;
   }
 
-  public function getProductsInOrders($orderIDS = null) {
+  public function getProductsInOrders($data = null) {
     global $wpdb;
 
-    if ($orderIDS == null) {
-      $orderIDS = $this->orderIDs;
+    if ($data == null) {
+      $data = $this->orderIDs;
     }
 
-    $orderIDSAsString = implode(',', $orderIDS);
+    $orderIDs = array_map(function($orderID) {
+      return $orderID["order_id"];
+    }, $data);
 
-    $query = "SELECT * FROM {$wpdb->prefix}woocommerce_order_items where order_id IN ($orderIDSAsString) AND order_item_type = 'line_item'";
+    $orderIDSAsString = implode(',', $orderIDs);
+
+    $query = "SELECT {$wpdb->prefix}woocommerce_order_items.order_item_id, {$wpdb->prefix}woocommerce_order_items.order_item_name AS product_name, {$wpdb->prefix}woocommerce_order_items.order_id, {$wpdb->prefix}postmeta.meta_value as order_sold_at FROM {$wpdb->prefix}woocommerce_order_items
+              JOIN {$wpdb->prefix}postmeta ON {$wpdb->prefix}woocommerce_order_items.order_id = {$wpdb->prefix}postmeta.post_id AND {$wpdb->prefix}postmeta.meta_key = '_order_sold'
+              where order_id
+              IN ($orderIDSAsString)
+              AND order_item_type = 'line_item';";
+
+
+    // echo $query;
     $results = $wpdb->get_results($query, ARRAY_A);
-
+    $this->orders = $results;
     $orderItemIDS = array_map(function($orderItemID) {
       return $orderItemID["order_item_id"];
     }, $results);
@@ -50,26 +61,28 @@ class Abrechnung {
     }
 
     $orderItemIDsAsString = implode(',', $orderItemIDs);
-    $query = "SELECT wp_mokkamerch_artistProducts.artist_id, wp_mokkamerch_artistProducts.artist_name,wp_mokkamerch_woocommerce_order_itemmeta.order_item_id, wp_mokkamerch_artistProducts.product_id , wp_mokkamerch_woocommerce_order_itemmeta.meta_value AS variation_id FROM wp_mokkamerch_woocommerce_order_itemmeta JOIN wp_mokkamerch_artistProducts ON wp_mokkamerch_woocommerce_order_itemmeta.meta_value = wp_mokkamerch_artistProducts.variation_id WHERE (meta_key = '_variation_id') AND order_item_id IN ($orderItemIDsAsString)";
+    $query = "SELECT {$wpdb->prefix}woocommerce_order_items.order_id,{$wpdb->prefix}artistProducts.artist_id, {$wpdb->prefix}artistProducts.artist_name, MAIN_ORDER_ITEMMETA.order_item_id, {$wpdb->prefix}artistProducts.product_id , MAIN_ORDER_ITEMMETA.meta_value AS variation_id, LINE_TOTAL_ORDER_ITEMMETA.meta_value AS line_price, LINE_TAX_ORDER_ITEMMETA.meta_value AS line_tax
+        FROM {$wpdb->prefix}woocommerce_order_itemmeta AS MAIN_ORDER_ITEMMETA
+        JOIN {$wpdb->prefix}woocommerce_order_itemmeta AS LINE_TOTAL_ORDER_ITEMMETA
+          ON MAIN_ORDER_ITEMMETA.order_item_id = LINE_TOTAL_ORDER_ITEMMETA.order_item_id AND LINE_TOTAL_ORDER_ITEMMETA.meta_key = '_line_total'
+        JOIN {$wpdb->prefix}woocommerce_order_itemmeta AS LINE_TAX_ORDER_ITEMMETA
+          ON MAIN_ORDER_ITEMMETA.order_item_id = LINE_TAX_ORDER_ITEMMETA.order_item_id AND LINE_TAX_ORDER_ITEMMETA.meta_key = '_line_tax'
+        JOIN {$wpdb->prefix}artistProducts
+          ON MAIN_ORDER_ITEMMETA.meta_value = {$wpdb->prefix}artistProducts.variation_id
+        JOIN {$wpdb->prefix}woocommerce_order_items ON MAIN_ORDER_ITEMMETA.order_item_id = {$wpdb->prefix}woocommerce_order_items.order_item_id
+        WHERE (MAIN_ORDER_ITEMMETA.meta_key = '_variation_id') AND MAIN_ORDER_ITEMMETA.order_item_id IN ($orderItemIDsAsString)";
     // echo $query;
 
     $results = $wpdb->get_results($query, ARRAY_A);
-    // echo "<pre>";
-    // print_r($results);
-    // echo "</pre>";
 
     self::nonEmptyArtistBills($results);
-    // echo "<pre>";
-    // print_r($uniqueArtists);
-    // echo "</pre>";
+
   }
 
   public function nonEmptyArtistBills($data) {
     $uniqueArtists = array_unique(array_column($data, 'artist_name'));
     array_map(function($artist) {
       $this->artists[$artist] = [];
-      // $this->artists[$artist]["soldProducts"] = [];
-      // $this->artists[$artist]["artistDetails"] = [];
     }, $uniqueArtists);
 
     foreach ($uniqueArtists as $key => $artistName) {
@@ -85,9 +98,6 @@ class Abrechnung {
     $query = "SELECT {$wpdb->prefix}artists.artist_name, {$wpdb->prefix}artists.init_bill_number, {$wpdb->prefix}artists.vorname, {$wpdb->prefix}artists.nachname, {$wpdb->prefix}terms.term_id FROM {$wpdb->prefix}artists JOIN {$wpdb->prefix}terms ON {$wpdb->prefix}artists.slug = {$wpdb->prefix}terms.slug";
 
     $results = $wpdb->get_results($query, ARRAY_A);
-    // echo "<pre>";
-    // print_r($results);
-    // echo "</pre>";
 
   }
 
@@ -104,9 +114,18 @@ class Abrechnung {
         // _article_type
         //_product_production
         // _thumbnail_id
-        $query = $wpdb->prepare("SELECT meta_value AS productId FROM {$wpdb->prefix}postmeta WHERE post_id = %d AND meta_key = '_product_production'", $artistData["products"][$i]["product_id"]);
+
+        $productID = $artistData["products"][$i]["product_id"];
+        $orderID = $artistData["products"][$i]["order_id"];
+        $query = $wpdb->prepare("SELECT meta_value AS productId FROM {$wpdb->prefix}postmeta WHERE post_id = %d AND meta_key = '_product_production'", $productID);
         $result = $wpdb->get_results($query, ARRAY_A);
         $this->artists[$artistName]["products"][$i]["productProduction"] = $result[0]["productId"];
+
+        // find value of order_id in $this->orderIDs
+        // to add order_sold_at
+        $key = array_search($orderID, array_column($this->orderIDs, 'order_id'));
+        $this->artists[$artistName]["products"][$i]["order_sold_at"] = $this->orderIDs[$key]["order_sold_at"];
+
       }
     }
   }
@@ -114,25 +133,46 @@ class Abrechnung {
   public function getVariationData($data = null) {
     global $wpdb;
 
-    if ($data = null) {
+    if ($data == null) {
       $data = $this->artists;
     }
 
     foreach ($data as $artistName => $artistData) {
       $productCount = count($artistData["products"]);
       for($i = 0; $i < $productCount; $i++) {
-        // attribute_pa_groesse
-        // attribute_pa_farbe
-        // attribute_pa_modell
-        $query = $wpdb->prepare("SELECT meta_value AS productId FROM {$wpdb->prefix}postmeta WHERE post_id = %d AND meta_key = '_product_production'", $artistData["products"][$i]["variation_id"]);
+        $variationID = $artistData["products"][$i]["variation_id"];
+        $query = $wpdb->prepare("SELECT MAIN_POSTMETA.meta_value AS variation_id, ROHARTIKEL_META.meta_value as rohartikelnr FROM {$wpdb->prefix}postmeta AS MAIN_POSTMETA
+          JOIN {$wpdb->prefix}postmeta AS ROHARTIKEL_META ON MAIN_POSTMETA.post_id = ROHARTIKEL_META.post_id AND ROHARTIKEL_META.meta_key = '_rohartikel_default'
+          WHERE MAIN_POSTMETA.post_id = %d ORDER BY MAIN_POSTMETA.meta_id LIMIT 1", $variationID);
+          // echo $query;
         $result = $wpdb->get_results($query, ARRAY_A);
-        $this->artists[$artistName]["products"][$i]["productProduction"] = $result[0]["productId"];
+        $rohartikelNummer = $result[0]["rohartikelnr"];
+        $this->artists[$artistName]["products"][$i]["rohartikelnr"] = $rohartikelNummer;
+
+        $query = $wpdb->prepare("SELECT * FROM {$wpdb->prefix}rohartikel WHERE rohartikel_nr_extern = %d", $rohartikelNummer);
+        $rohartikelData = $wpdb->get_results($query,ARRAY_A);
+        $this->artists[$artistName]["products"][$i]["color"] = $rohartikelData[0]["farbe"];
+        $this->artists[$artistName]["products"][$i]["size"] = $rohartikelData[0]["groesse"];
+        $this->artists[$artistName]["products"][$i]["quality"] = $rohartikelData[0]["qualitaet"];
+        $this->artists[$artistName]["products"][$i]["article_type"] = $rohartikelData[0]["artikel_name"];
+        $this->artists[$artistName]["products"][$i]["basis_preis"] = $rohartikelData[0]["basis_preis"];
       }
     }
-
-
   }
 
+  public function addMarge(&$data = null) {
+    if ($data == null) {
+      $data = $this->artists;
+    }
+
+    foreach ($data as $artistName => $artistData) {
+      $productCount = count($artistData["products"]);
+      for($i = 0; $i < $productCount; $i++) {
+
+        calculateMarge($this->artists[$artistName]["products"][$i]);
+      }
+    }
+  }
 }
 
 function groupSameArtistIDs($dataArray, $singleID) {
@@ -143,4 +183,28 @@ function groupSameArtistIDs($dataArray, $singleID) {
     }
   }
   return $array;
+}
+
+function calculateMarge(&$singleProductArray) {
+  $netto = false;
+  $onDemand = true;
+  if (isset($singleProductArray["netto"])) {
+    $netto = $singleProductArray["netto"];
+  }
+
+
+
+  if ($singleProductArray["productProduction"] == 'sieb') {
+    $onDemand = false;
+  }
+  $onDemand ? calculateMargeOnDemand($singleProductArray) : calculateMargeSieb($singleProductArray);
+
+}
+
+function calculateMargeOnDemand(&$singleProductArray) {
+  $singleProductArray["marge"] = (($singleProductArray["line_price"] + $singleProductArray["line_tax"] - $singleProductArray["basis_preis"] ) / 1.19) ;
+}
+
+function calculateMargeSieb(&$singleProductArray) {
+
 }
--- includes/Table-Artist-Product.php
+++ includes/Table-Artist-Product.php
@@ -36,3 +36,7 @@ class TableArtistProduct {
     }
   }
 }
+
+
+// add rohartikelnummern
+// basispreistabelle für künstler
